3a7912df00f24d85c8bf87c1441c1415c96fcd5e
riparuk <faruqirifa@gmail.com>
2025-02-12 20:03:36 +0700

    Init project with simple user and book database

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class BookController extends Controller
+{
+    public function index()
+    {
+        $books = Book::all();
+        return view('books.index', compact('books'));
+    }
+
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'author' => 'required',
+            'publisher' => 'required',
+            'year' => 'required|integer',
+            'description' => 'required',
+        ]);
+
+        Book::create($request->all());
+        return redirect()->route('books.index');
+    }
+
+    public function edit(Book $book)
+    {
+        return view('books.edit', compact('book'));
+    }
+
+    public function update(Request $request, Book $book)
+    {
+        $request->validate([
+            'title' => 'required',
+            'author' => 'required',
+            'publisher' => 'required',
+            'year' => 'required|integer',
+            'description' => 'required',
+        ]);
+
+        $book->update($request->all());
+        return redirect()->route('books.index');
+    }
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
+        return redirect()->route('books.index');
+    }
+}
